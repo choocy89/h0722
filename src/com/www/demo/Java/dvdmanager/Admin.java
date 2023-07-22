1515ecfcd76d0b6e8ab15c0d0adf0ff4ed41c5e1
choocy89 <1748900258@qq.com>
2023-07-22 09:54:38 +0800

    lkhoiho

---
+++ src/com/www/demo/Java/dvdmanager/Admin.java
@@ -0,0 +1,13 @@
+package com.www.demo.Java.dvdmanager;
+
+/**
+ * @author Choccy
+ * @data 2023/5/16 9:37
+ */
+public class Admin {
+    public String adminName;
+    public String adminPass;
+
+    public void setAdminname(String string) {
+    }
+}
